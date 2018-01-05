3960204affc6ffd67215cd3bc86943a5336b0c86
Flavius12 <info@evolsoft.tk>
2018-01-05 12:40:58 +0100

    Updated to PocketMine-MP 1.7dev

--- README.md
+++ README.md
@@ -1,7 +1,8 @@
 ![start2](https://cloud.githubusercontent.com/assets/10303538/6315586/9463fa5c-ba06-11e4-8f30-ce7d8219c27d.png)
 
 # LimitHeight
-A PocketMine-MP plugin to limit the build height
+
+A PocketMine-MP plugin to limit the build height on your server.
 
 ## Category
 
@@ -9,26 +10,29 @@ PocketMine-MP plugins
 
 ## Requirements
 
-PocketMine-MP Alpha_1.4 API 1.11.0<br>
+PocketMine-MP 1.7dev API 3.0.0-ALPHA7, 3.0.0-ALPHA8, 3.0.0-ALPHA9
 
 ## Overview
 
-
-**LimitHeight** allows you to limit the build height in your worlds.
+**LimitHeight** let you limit the build height on your server.
 
 **EvolSoft Website:** http://www.evolsoft.tk
 
 **This Plugin uses the New API. You can't install it on old versions of PocketMine.**
 
-With LimitHeight you can limit the build height in worlds (read documentation)
+With LimitHeight you can limit the build height on worlds (read documentation).
+
+## Donate
+
+Support the development of this plugin with a small donation by clicking [:dollar: here](https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=flavius.c.1999@gmail.com&lc=US&item_name=www.evolsoft.tk&no_note=0&cn=&curency_code=EUR&bn=PP-DonationsBF:btn_donateCC_LG.gif:NonHosted). Thank you :smile:
 
 ## Documentation
 
-Configuration (config.yml):
+**Configuration (config.yml):**
 
 ```yaml
 ---
-#Height limit (by default ops have limitheight.bypass permission set and they can bypass the limit. If you want to limit the build height also for ops, simply use a permission manager plugin)
+#Height limit (by default ops have limitheight.bypass permission set and they can bypass the limit)
 height-limit: 64
 #Show message on limit reached
 show-message: true
@@ -47,5 +51,5 @@ disabled-in-worlds: []
 
 **Permissions:**
 
-- <dd><i><b>limitheight.*</b> - LimitHeight permissions.</i></dd>
-- <dd><i><b>limitheight.bypass</b> - Allows players to bypass the height limit.</i></dd>
+- <dd><i><b>limitheight.*</b> - LimitHeight permissions tree.</i></dd>
+- <dd><i><b>limitheight.bypass</b> - Let players bypass the height limit.</i></dd>
---
+++ src/LimitHeight/LimitHeight.php
@@ -0,0 +1,105 @@
+<?php
+
+/*
+ * LimitHeight (v1.2) by EvolSoft
+ * Developer: EvolSoft (Flavius12)
+ * Website: https://www.evolsoft.tk
+ * Date: 05/01/2018 11:27 AM (UTC)
+ * Copyright & License: (C) 2015-2018 EvolSoft
+ * Licensed under MIT (https://github.com/EvolSoft/LimitHeight/blob/master/LICENSE)
+ */
+
+namespace LimitHeight;
+
+use pocketmine\event\block\BlockPlaceEvent;
+use pocketmine\event\Listener;
+use pocketmine\plugin\PluginBase;
+use pocketmine\Server;
+use pocketmine\utils\TextFormat;
+
+class LimitHeight extends PluginBase implements Listener {
+    
+    /** @var string */
+    const PREFIX = "&3[LimitHeight] ";
+    
+    /**
+     * Translate Minecraft colors
+     * @param string $symbol
+     * @param string $message
+     * @return string
+     */
+    public function translateColors($symbol, $message){
+        
+        $message = str_replace($symbol . "0", TextFormat::BLACK, $message);
+        $message = str_replace($symbol . "1", TextFormat::DARK_BLUE, $message);
+        $message = str_replace($symbol . "2", TextFormat::DARK_GREEN, $message);
+        $message = str_replace($symbol . "3", TextFormat::DARK_AQUA, $message);
+        $message = str_replace($symbol . "4", TextFormat::DARK_RED, $message);
+        $message = str_replace($symbol . "5", TextFormat::DARK_PURPLE, $message);
+        $message = str_replace($symbol . "6", TextFormat::GOLD, $message);
+        $message = str_replace($symbol . "7", TextFormat::GRAY, $message);
+        $message = str_replace($symbol . "8", TextFormat::DARK_GRAY, $message);
+        $message = str_replace($symbol . "9", TextFormat::BLUE, $message);
+        $message = str_replace($symbol . "a", TextFormat::GREEN, $message);
+        $message = str_replace($symbol . "b", TextFormat::AQUA, $message);
+        $message = str_replace($symbol . "c", TextFormat::RED, $message);
+        $message = str_replace($symbol . "d", TextFormat::LIGHT_PURPLE, $message);
+        $message = str_replace($symbol . "e", TextFormat::YELLOW, $message);
+        $message = str_replace($symbol . "f", TextFormat::WHITE, $message);
+        
+        $message = str_replace($symbol . "k", TextFormat::OBFUSCATED, $message);
+        $message = str_replace($symbol . "l", TextFormat::BOLD, $message);
+        $message = str_replace($symbol . "m", TextFormat::STRIKETHROUGH, $message);
+        $message = str_replace($symbol . "n", TextFormat::UNDERLINE, $message);
+        $message = str_replace($symbol . "o", TextFormat::ITALIC, $message);
+        $message = str_replace($symbol . "r", TextFormat::RESET, $message);
+        
+        return $message;
+    }
+    
+    public function onEnable(){
+        @mkdir($this->getDataFolder());
+        $this->saveDefaultConfig();
+        $logger = Server::getInstance()->getLogger();
+        $logger->info($this->translateColors("&", self::PREFIX . "&9LimitHeight &bv" . $this->getDescription()->getVersion() . "&9 developed by &bEvolSoft"));
+        $logger->info($this->translateColors("&", self::PREFIX . "&9Website &b" . $this->getDescription()->getWebsite()));
+        $this->getServer()->getPluginManager()->registerEvents($this, $this);
+    }
+    
+    /**
+     * Check if the height limit is disabled on the specified world
+     * @param string
+     * @return bool
+     */
+    private function isLimitDisabled($level) : bool {
+        $cfg = $this->getConfig()->getAll();
+        foreach($cfg["disabled-in-worlds"] as $item){
+            if(strcasecmp($item, $level) == 0){
+                return true;
+            }
+        }
+        return false;
+    }
+    
+    /**
+     * @param BlockPlaceEvent $event
+     */
+    public function onBlockPlace(BlockPlaceEvent $event){
+        $player = $event->getPlayer();
+        $block = $event->getBlock();
+        $cfg = $this->getConfig()->getAll();
+        if(!$player->hasPermission("limitheight.bypass") && !$this->isLimitDisabled($player->getLevel()->getName())){
+            if($block->y > $cfg["height-limit"]){
+                if($cfg["show-message"]){
+                    $message = $cfg["message"];
+                    $message = str_replace("{LIMIT}", $cfg["height-limit"], $message);
+                    if($cfg["show-prefix"]){
+                        $message = self::PREFIX . $message;
+                    }
+                    $player->sendMessage($this->translateColors("&", $message));
+                }
+                $event->setCancelled(true);
+            }
+        }
+    }
+}
